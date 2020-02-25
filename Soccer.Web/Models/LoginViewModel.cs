c5c3fe407f06d1e6621c3ccd224134941e387126
dacalo <dacalo.soporte@gmail.com>
2020-02-24 20:26:17 -0600

    Login & Logout web

---
+++ Soccer.Web/Models/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Soccer.Web.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Usuario")]
+        public string Username { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+        [Display(Name = "Recordarme")]
+        public bool RememberMe { get; set; }
+    }
+}
